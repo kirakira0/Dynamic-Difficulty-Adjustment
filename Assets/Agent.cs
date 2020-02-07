cb3e0b0f08c39f1b1dd76e2533bd7007f2d95794
kirakira0 <kiratoal0@gmail.com>
2020-02-06 17:15:02 -0800

    Controlled platform generation with constants

--- Assets/Agent.cs
+++ Assets/Agent.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Agent : MonoBehaviour
 {
+    //public ObjectPooler[] platformPool; 
+    public GameObject[] platforms; 
+    public GameObject player; 
+    public GameObject platformGenerationPoint; 
+
+    private float nextActionTime = 0.0f;
+    public float period = 0.1f;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+ 
+    void Update () {
+        if (Time.time > nextActionTime ) {
+            nextActionTime += period;
+            Instantiate(platforms[0], new Vector3(platformGenerationPoint.transform.position.x, 0, 0), Quaternion.identity);
+            // execute block of code here
+        }
     }
+
 }
