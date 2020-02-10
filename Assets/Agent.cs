3c350eef856a7de1ea5f877c3e73c95e70d925ef
kirakira0 <kiratoal0@gmail.com>
2020-02-10 12:55:05 -0800

    Platform types and heights now manually entered

--- Assets/Agent.cs
+++ Assets/Agent.cs
@@ -10,22 +10,59 @@ public class Agent : MonoBehaviour
     public GameObject platformGenerationPoint; 
 
     private float nextActionTime = 0.0f;
-    public float period = 0.1f;
+    public float period = 0.5f;
+
+    public Vector3 low; 
+    public Vector3 mid; 
+    public Vector3 high;  
+
+    public GameObject shortPlatform;
+    public GameObject mediumPlatform; 
+    public GameObject longPlatform;  
     
+    private int i = 0; 
+    private int height; 
+
+    private List<GameObject> type = new List<GameObject>(); 
+    //private List<Vector3> position = new List<Vector3>(); 
+    private List<int> position = new List<int>(); 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shortPlatform = platforms[0];
+        mediumPlatform = platforms[1];
+        longPlatform = platforms[2]; 
+
+        type.AddRange(new GameObject[] {
+            shortPlatform, shortPlatform, shortPlatform, mediumPlatform, mediumPlatform, mediumPlatform, longPlatform, longPlatform, longPlatform
+        });
+
+        position.AddRange(new int[] {
+            -3, 0, 3, -3, 0, 3, -3, 0, 3
+        }); 
+ 
+
+        InvokeRepeating("RepeatedGeneration", 1.0f, 1.0f);
+
+
     }
 
 
- 
     void Update () {
-        if (Time.time > nextActionTime ) {
-            nextActionTime += period;
-            Instantiate(platforms[0], new Vector3(platformGenerationPoint.transform.position.x, 0, 0), Quaternion.identity);
-            // execute block of code here
-        }
+        
+    }
+
+    void RepeatedGeneration()
+    {
+        //instantiate creates the new platforms 
+        Instantiate(type[i], new Vector3(platformGenerationPoint.transform.position.x, position[i], 0), Quaternion.identity);
+        //log the updates platform generation position 
+        Debug.Log(platformGenerationPoint.transform.position.x);
+        //increment the index to work through the list 
+        i++; 
+
     }
 
 }
