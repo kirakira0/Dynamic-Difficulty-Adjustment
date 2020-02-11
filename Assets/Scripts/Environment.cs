63d3bf74006ca20105b0b17f7f3b9bf05299d613
kirakira0 <kiratoal0@gmail.com>
2020-02-10 16:29:38 -0800

    Environment script

--- Assets/Scripts/Environment.cs
+++ Assets/Scripts/Environment.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Agent : MonoBehaviour
+public class Environment : MonoBehaviour
 {
     //public ObjectPooler[] platformPool; 
     public GameObject[] platforms; 
