8b8b2024ef10c07e9e76d33dcf2491d9fd62c089
kirakira0 <kiratoal0@gmail.com>
2020-02-13 16:37:24 -0800

    Generation now treated like a method

--- Assets/Scripts/Environment.cs
+++ Assets/Scripts/Environment.cs
@@ -12,9 +12,13 @@ public class Environment : MonoBehaviour
     private float nextActionTime = 0.0f;
     public float period = 0.5f;
 
-    public Vector3 low; 
-    public Vector3 mid; 
-    public Vector3 high;  
+    // public Vector3 low; 
+    // public Vector3 mid; 
+    // public Vector3 high; 
+
+    public int high = 3; 
+    public int mid = 0; 
+    public int low = -3;  
 
     public GameObject shortPlatform;
     public GameObject mediumPlatform; 
@@ -35,17 +39,9 @@ public class Environment : MonoBehaviour
         mediumPlatform = platforms[1];
         longPlatform = platforms[2]; 
 
-        type.AddRange(new GameObject[] {
-            shortPlatform, shortPlatform, shortPlatform, mediumPlatform, mediumPlatform, mediumPlatform, longPlatform, longPlatform, longPlatform
-        });
-
-        position.AddRange(new int[] {
-            -3, 0, 3, -3, 0, 3, -3, 0, 3
-        }); 
+        Generate("short", "mid"); 
  
-
-        InvokeRepeating("RepeatedGeneration", 1.0f, 1.0f);
-
+        //InvokeRepeating("RepeatedGeneration", 1.0f, 1.0f);
 
     }
 
@@ -54,15 +50,39 @@ public class Environment : MonoBehaviour
         
     }
 
-    void RepeatedGeneration()
-    {
-        //instantiate creates the new platforms 
-        Instantiate(type[i], new Vector3(platformGenerationPoint.transform.position.x, position[i], 0), Quaternion.identity);
-        //log the updates platform generation position 
-        Debug.Log(platformGenerationPoint.transform.position.x);
-        //increment the index to work through the list 
-        i++; 
+    // void RepeatedGeneration()
+    // {
+    //     //instantiate creates the new platforms 
+    //     Instantiate(type[i], new Vector3(platformGenerationPoint.transform.position.x, position[i], 0), Quaternion.identity);
+    //     //log the updates platform generation position 
+    //     Debug.Log(platformGenerationPoint.transform.position.x);
+    //     //increment the index to work through the list 
+    //     i++; 
 
+    // }
+
+    void Generate(string platform, string height) {
+        int ypos = 0; 
+        if (height == "low") {
+            ypos = -3; 
+        }
+        else if (height == "high") {
+            ypos = 3; 
+        }
+        else if (height == "mid") {
+            ypos = 0; 
+        }
+        GameObject type = shortPlatform; 
+        if (platform == "short") {
+            type = shortPlatform; 
+        }
+        else if (platform == "medium") {
+            type = mediumPlatform;
+        }
+        else if (platform == "long") {
+            type = longPlatform; 
+        }
+        Instantiate(type, new Vector3(platformGenerationPoint.transform.position.x, ypos, 0), Quaternion.identity);
     }
 
 }
