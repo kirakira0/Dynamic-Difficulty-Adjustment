66bb94864e5a27bf7bf602e52b4e710aa8d11463
kirakira0 <kiratoal0@gmail.com>
2020-02-20 17:02:00 -0800

    temporarily locked speech + increased player mass

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class Agent : MonoBehaviour
 {
+    private int i = 0; 
+
     public Environment Environment; 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(Environment.test); 
-        InvokeRepeating("RepeatCallToEnv", 1.0f, 2f);
-        //acclimation --> don't even awtich before a minimum number of iterations, stable coing collection percetage 
+        InvokeRepeating("RepeatCallToEnv", 1.0f, 1.5f);
+        //acclimation --> don't even swtich before a minimum number of iterations, stable coing collection percetage 
     }
 
     // Update is called once per frame
@@ -20,6 +22,16 @@ public class Agent : MonoBehaviour
     }
 
     void RepeatCallToEnv() {
-        Environment.Generate("m", "mid"); 
+        if (i == 0) {
+            Environment.Generate("m", "low"); 
+        }
+        if (i == 1) {
+            Environment.Generate("m", "mid"); 
+        }
+        if (i == 2) {
+            Environment.Generate("m", "high"); 
+            i = -1; 
+        }
+        i += 1; 
     }
 }
