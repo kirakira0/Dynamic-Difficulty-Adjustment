3dc60c6812d84c5c0eceafb269e5e733255ff1ac
kirakira0 <kiratoal0@gmail.com>
2020-09-24 23:08:27 -0700

    Simple web request

---
+++ Assets/Scripts/Web.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+// UnityWebRequest.Get example
+
+// Access a website and use UnityWebRequest.Get to download a page.
+// Also try to download a non-existing page. Display the error.
+
+public class Web : MonoBehaviour
+{
+
+    void Start()
+    {
+        // A correct website page.
+        // Use coroutine b/c we need to wait for a reply from the server (can't be done in 1 frame).
+        StartCoroutine(GetRequest("http://localhost:8081/DDA/GetDate.php"));
+
+        // A non-existing page.
+        StartCoroutine(GetRequest("https://error.html")); 
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+            }
+        }
+    }
+}
