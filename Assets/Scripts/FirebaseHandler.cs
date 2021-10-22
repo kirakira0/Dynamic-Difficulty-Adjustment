66cb92b50c5559e7ebdba8216797b07e02160b7f
kirakira0 <kiratoal0@gmail.com>
2021-10-22 11:18:14 -0700

    Updated randomized player id

--- Assets/Scripts/FirebaseHandler.cs
+++ Assets/Scripts/FirebaseHandler.cs
@@ -5,6 +5,10 @@ using static UnityEngine.Random;
 using UnityEngine.UI;
 using Proyecto26;
 
+/* 
+ *  https://github.com/proyecto26/RestClient
+ */
+
 public class FirebaseHandler : MonoBehaviour
 {
     public Text scoreText;
@@ -13,27 +17,25 @@ public class FirebaseHandler : MonoBehaviour
     public static string playerName;
     public InputField nameText;
 
+    private Manager Manager;
+
+    public string api = "https://dynamic-diff-adjustment-dev-default-rtdb.firebaseio.com/";
+
     // Start is called before the first frame update
     void Start()
     {
+        Manager = GameObject.Find("Manager").GetComponent<Manager>();
         // Generate random player id 
-        id = Random.Range(0, 101);
-        // playerScore = Random.Next(0, 101); 
-        // playerScore = Random.Range(0, 101);
-        // scoreText.text = "Score: " + playerScore; 
-    }
-
-    public void OnSubmit() {
-        // playerName = nameText.text;
-
-        // PostToDatabase();
+        id = Random.Range(0, 10001); 
     }
 
     public void PostToDatabase(string gameData) {
+
         Player user = new Player(gameData);
         // RestClient.Put("https://dynamic-diff-adjustment-dev-default-rtdb.firebaseio.com/" + playerName + ".json", user);
         Debug.Log("PRE POST");
-        RestClient.Put("https://dynamic-diff-adjustment-dev-default-rtdb.firebaseio.com/" + "TESTPLAYER" + ".json", user);
+        RestClient.Put(api + Manager.PlayerData.GetPlayerId() + ".json", user);
+        
         Debug.Log("POST POST");
 
     }
--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour
 {
+    public PlayerData PlayerData;
+    public Text idAndInstructionsText;
+
     public Text metricsText; 
     public GameObject startingPlatform; 
     public Transform startingPlayerPoint; 
@@ -28,6 +31,12 @@ public class Manager : MonoBehaviour
     private float xpos;  
 
     void Awake() {
+        PlayerData = new PlayerData(Random.Range(0, 10001)); 
+        idAndInstructionsText.text = "Thank you for participating in our research trial. Your player id is: " + PlayerData.GetPlayerId() +
+                                      ". Please save this number as it will be asked about in the player feedback form.\n\n The game that you are about to play is a platformer. To play, press the [ENTER] key or [SPACE] key to jump. You may “double jump”, or press the jump keys again while in the air to jump a second time. The longer a jump key is held, the higher the character will jump. You may wish to leverage this scaling jump force when you encounter large gaps between platforms or small gaps that require short jumps. The objective is to jump from platform to platform and collect as many coins as possible. Falling between the platforms will count as a death. The game will conclude after a certain number of deaths or after the completion of ten rounds." + 
+                                      "\n\nTo start moving, press one of the two jump keys. ";
+
+
         LOGGER = GameObject.Find("Logger").GetComponent<Logger>(); 
         PlayerController = GameObject.Find("Player").GetComponent<PlayerController>();
         Generator = GameObject.Find("PlatformGenerator").GetComponent<Generator>(); 
@@ -84,4 +93,25 @@ public class Manager : MonoBehaviour
         gameIsPaused = paused;
     }
 
+}
+
+
+
+
+public class PlayerData  {
+
+    private int playerId; 
+    private string gameData;
+
+    public PlayerData(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public void SetGameData(string gameData) {
+        this.gameData = gameData;
+    }
+
+    public int GetPlayerId() {
+        return this.playerId;
+    }
 }
